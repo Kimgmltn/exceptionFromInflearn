84b03b1544218d734b10d59fadbc68a2147d0c36
Kimgmltn <Kimgmltn0323@gmail.com>
2023-07-30 20:39:04 +0900

    setting

---
+++ src/main/java/hello/exceptionfrominflearn/ExceptionFromInflearnApplication.java
@@ -0,0 +1,13 @@
+package hello.exceptionfrominflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionFromInflearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionFromInflearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/exceptionfrominflearn/ExceptionFromInflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.exceptionfrominflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionFromInflearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
